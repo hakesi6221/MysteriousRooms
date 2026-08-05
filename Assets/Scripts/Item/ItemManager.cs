0af9c61e9e7b2d3698ad4df6b6b05c7ae585daa4
hakesi6221 <yuanjianren000@gmail.com>
2026-08-05 18:32:59 +0900

    [Update] ItemManagerのアイテムリスト直接参照を廃止

--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// 現在所持しているアイテムを補完するシングルトンクラス
@@ -12,7 +13,38 @@ public class ItemManager : SingletonMonoBehaviour<ItemManager>
     private List<ItemBase> _havingItemList = new List<ItemBase>();
 
     /// <summary>
-    /// 持っているアイテムのリスト
+    /// アイテムの入手、リストへの追加
     /// </summary>
-    public List<ItemBase> HavingItemList { get { return _havingItemList; } }
+    /// <param name="item">アイテム情報</param>
+    public void AddItem(ItemBase item)
+    {
+        if (item == null)
+        {
+            Debug.LogError("{this.name}:追加アイテムが正しく渡されませんでした。");
+            return;
+        }
+        _havingItemList?.Add(item);
+    }
+
+    /// <summary>
+    /// 配列の番号から所持済みのアイテムの情報を取得する
+    /// </summary>
+    /// <param name="index">情報を取得したいアイテムのインデックス</param>
+    /// <returns></returns>
+    public ItemBase GetHavingItemByIndex(int index)
+    {
+        if (index < 0
+            || _havingItemList.Count <= index)
+        {
+            Debug.LogError($"{this.name}:配列の範囲外が指定されました。");
+            return null;
+        }
+
+        return _havingItemList[index];
+    }
+
+    /// <summary>
+    /// 所持アイテムの数
+    /// </summary>
+    public int ItemCount => _havingItemList.Count;
 }
