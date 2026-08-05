0965bc686e3144a9ea8680e717a18be32959b268
hakesi6221 <yuanjianren000@gmail.com>
2026-08-05 18:38:08 +0900

    [Add] カーソルアイコンの更新処理を実装するためのinterfaceを作成

---
+++ Assets/Scripts/Objects/IUpdateCursorObj.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// カーソルの更新処理を実装するインターフェース
+/// </summary>
+public interface IUpdateCursorObj
+{
+    /// <summary>
+    /// カーロスアイコンの更新
+    /// </summary>
+    void UpdateCursor();
+}
